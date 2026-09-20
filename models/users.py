fb1a25a43a8fd758c0b3d9954e94abd06e3e87b0
mahan-rezaei <mahanrezaei859@gmail.com>
2026-09-20 21:07:17 +0330

    add user model./

--- models/users.py
+++ models/users.py
@@ -1,5 +1,13 @@
-from sqlmodel import SQLModel
+from sqlmodel import SQLModel, Field
+from datetime import datetime
 
 
 class User(SQLModel, table=True):
-    pass
+    id: int | None = Field(default=None, primary_key=True)
+    full_name: str = Field(max_length=258)
+    username: str = Field(max_length=128, unique=True, index=True)
+    email: str = Field(unique=True, index=True)
+    password: str
+
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
